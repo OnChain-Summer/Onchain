7455d1356f2c51db66fe8f38454a5784adc70da7
vishalmet <vishalmet003@gmail.com>
2024-06-28 12:35:58 +0530

    buttom temp

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
+const ButtonTemp = ({ title, to }) => {
+  const navigate = useNavigate();
 
-const ButtonTemp = ({title}) => {
+  const handleClick = () => {
+    navigate(to);
+  };
+
   return (
     <div>
-      <div className=" flex justify-center pt-6 broge-font">
+      <div className="flex justify-center pt-6 broge-font">
         <motion.button
-          className=" text-white bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:from-yellow-500 hover:via-red-500 hover:to-pink-500 hover:shadow-xl  p-3 rounded text-xl font-medium border-b-2 border-white"
+          className="text-white bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:from-yellow-500 hover:via-red-500 hover:to-pink-500 hover:shadow-xl p-3 rounded text-base font-medium border-b-2 border-white"
           whileTap={{ scale: 0.9 }}
+          onClick={handleClick}
         >
           {title}
         </motion.button>
