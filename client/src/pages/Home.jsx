695166d363bcf5eeab9b382980aea64a0dbf323e
vishalmet <vishalmet003@gmail.com>
2024-06-30 18:59:34 +0530

    responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -42,12 +42,12 @@ const Home = () => {
             </div>
           </div>
           <div className=" items-center mb-10">
-            <p className=" text-4xl broge-font text-center text-white pt-16">
+            <p className=" text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 md:text-4xl broge-font text-center text-white mt-4 md:mt-0 p-4 md:p-10">
               How it Works
             </p>
-            <div className=" flex items-center pt-10">
+            <div className=" md:flex items-center pt-10">
               <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
-                <p className=" broge-font text-7xl">1</p>
+                <p className=" broge-font text-5xl md:text-7xl">1</p>
                 <p className=" text-2xl font-semibold">Create & Mint</p>
                 <p>
                   Transform your favorite memes into one-of-a-kind NFTs with our
@@ -55,7 +55,7 @@ const Home = () => {
                   yours.
                 </p>
               </div>
-              <div className="">
+              <div className=" hidden md:flex ">
                 <img
                   className=" max-w-96 max-h-72 rounded"
                   src={Meme1}
@@ -63,7 +63,7 @@ const Home = () => {
                 />
               </div>
               <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
-                <p className=" broge-font text-7xl">2</p>
+                <p className=" broge-font text-5xl md:text-7xl">2</p>
                 <p className=" text-2xl font-semibold">Collect & Trade</p>
                 <p>
                   Browse a vast collection of meme NFTs. Find classics like
@@ -71,23 +71,23 @@ const Home = () => {
                 </p>
               </div>
             </div>
-            <div className=" flex items-center justify-center mx-32">
-              <div className=" mx-2 md:mx-auto">
+            <div className=" md:flex items-center md:justify-center md:mx-32">
+              <div className="hidden md:flex mx-2 md:mx-auto">
                 <img
-                  className=" max-w-96 max-h-72 rounded"
+                  className="  max-w-96 max-h-72 rounded"
                   src={Meme2}
                   alt=""
                 />
               </div>
               <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
-                <p className=" broge-font text-7xl">3</p>
+                <p className=" broge-font text-5xl md:text-7xl">3</p>
                 <p className=" text-2xl font-semibold">Community Driven</p>
                 <p>
                   Join our lively community of meme lovers and NFT enthusiasts.
                   Share, discuss, and stay updated on the latest meme trends.
                 </p>
               </div>
-              <div className=" mx-2 md:mx-auto">
+              <div className="hidden md:flex mx-2 md:mx-auto">
                 <img
                   className=" max-w-96 max-h-72 rounded"
                   src={Meme3}
