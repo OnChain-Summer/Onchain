45222179f588eb53ea34c1ac197eb2f39ca77c30
vishalmet <vishalmet003@gmail.com>
2024-06-30 23:16:51 +0530

    minted

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,12 +41,15 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className=" items-center mb-10">
+        </GradientBackground>
+
+        
+        <div className=" bg-gradient-to-tr from-black via-gray-900 to-gray-600 items-center pb-10">
             <p className=" text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 md:text-4xl broge-font text-center text-white mt-4 md:mt-0 p-4 md:p-10">
               How it Works
             </p>
             <div className=" md:flex items-center pt-10">
-              <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
+              <div className="bg-transparent shadow-lg border-2 border-white lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
                 <p className=" broge-font text-5xl md:text-7xl">1</p>
                 <p className=" text-2xl font-semibold">Create & Mint</p>
                 <p>
@@ -62,7 +65,7 @@ const Home = () => {
                   alt=""
                 />
               </div>
-              <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
+              <div className="bg-transparent shadow-lg border-2 border-white lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
                 <p className=" broge-font text-5xl md:text-7xl">2</p>
                 <p className=" text-2xl font-semibold">Collect & Trade</p>
                 <p>
@@ -79,7 +82,7 @@ const Home = () => {
                   alt=""
                 />
               </div>
-              <div className="bg-transparent shadow-lg lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
+              <div className="bg-transparent shadow-lg border-2 border-white lg:backdrop-brightness-110 my-4 md:my-10 hover:bg-gradient-to-t hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white px-6 py-3 md:py-6 rounded-lg max-w-96 max-h-72 text-center mx-2 md:mx-auto">
                 <p className=" broge-font text-5xl md:text-7xl">3</p>
                 <p className=" text-2xl font-semibold">Community Driven</p>
                 <p>
@@ -97,9 +100,8 @@ const Home = () => {
             </div>
             <ButtonTemp title="Get Started -->" to="/mint" />
           </div>
-          <p className=" bg-black p-2 text-center text-white">© 2024 OnchainMemes. All rights reserved. </p>
-        </GradientBackground>
-      </div>
+          </div>
+          <p className=" bg-black p-3 bricolage-font text-center text-white">© 2024 OnchainMemes. All rights reserved. </p>
     </div>
   );
 };
